e10a89f9038f8b123d6b1a3e87d3c3fe80af7bf0
Shravani Madagala <shravani1805@gmail.com>
2021-06-12 12:30:09 +0530

    Packages

---
+++ Packages/src/Vehicle.java
@@ -0,0 +1,28 @@
+
+
+public class Vehicle {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hero hero = new Hero("Hero i-smart 110 2s", "MP65MD5060", "Ajay Singh", 90);
+		System.out.println(hero.getModelName());
+		System.out.println(hero.getOwnerName());
+		System.out.println(hero.getRegistrationNumber());
+		System.out.println(hero.getSpeed()+" kmph");
+		hero.radio();
+		
+		System.out.println();
+		
+		Honda honda = new Honda("Honda CB Hornet 160R", "M15FZ5101", "Vijay Singh", 110);
+		System.out.println(honda.getModelName());
+		System.out.println(honda.getOwnerName());
+		System.out.println(honda.getRegistrationNumber());
+		System.out.println(honda.getSpeed()+" kmph");
+		honda.cdplayer();	
+	}
+}
+abstract class Vehicle1 {
+	
+	public abstract String getModelName();
+	public abstract String getRegistrationNumber(); 
+	public abstract String getOwnerName(); 
+}
